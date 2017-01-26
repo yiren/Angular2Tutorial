04c95e25a04216611c549e45996490f96c502b2b
yiren <yiren.goo@gmail.com>
2017-01-26 11:24:20 +0800

    handle click event with Subject

--- src/app/ngrx/ngrx.component.ts
+++ src/app/ngrx/ngrx.component.ts
@@ -3,6 +3,7 @@ import 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 
 import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject'
 
 @Component({
   selector: 'app-ngrx',
@@ -11,12 +12,15 @@ import {Observable} from 'rxjs/Observable';
 })
 export class NgrxComponent implements OnInit {
 
-  clock=Observable
-        .interval(1000)
-        .map(()=>new Date());
+  clock$=new Subject();
+  clock:Observable<Date>;
+  // clock=Observable
+  //       .interval(1000)
+  //       .map(()=>new Date());
 
   constructor() {
-    this.clock.subscribe(console.log);
+    //this.clock.subscribe(console.log);
+    this.clock=this.clock$.map(()=>new Date());
   }
 
   ngOnInit() {
